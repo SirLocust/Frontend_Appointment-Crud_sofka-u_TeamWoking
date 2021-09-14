3d77f1ef6e3f67fcd26663432d39f1b9704e2dfa
Camilo U <cauribeva@unal.edu.co>
2021-09-14 18:56:20 -0500

    Agregado componente CreateCardAppointment

--- src/components/CreateCardAppointment.tsx
+++ src/components/CreateCardAppointment.tsx
@@ -1,12 +1,39 @@
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { Appointment } from './interfaces/Appointment'
 
 export default function CreateCardAppointment() {
-  const [appointment, setappointment] = React.useState<string>('')
+  const [appointment, setAppointment] = React.useState<Appointment>({
+    date: '',
+    nameDoctor: '',
+    namePatient: '',
+    motiveConsulta: '',
+    building: '',
+  })
 
-  const handleAdd = () => {
-    setappointment(appointment)
+  const { register, handleSubmit } = useForm<Appointment>()
+
+  const onSubmit = (data: Appointment) => {
+    console.log(data)
+
+    setAppointment(data)
   }
-  handleAdd()
 
-  return <form action=""></form>
+  console.log(appointment)
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Doctor</label>
+      <input {...register('nameDoctor')} />
+      <label>Paciente</label>
+      <input {...register('namePatient')} />
+      <label>Motivo</label>
+      <input {...register('motiveConsulta')} />
+      <label>Lugar</label>
+      <input {...register('building')} />
+      <button type="submit">Enviar</button>
+    </form>
+  )
 }
