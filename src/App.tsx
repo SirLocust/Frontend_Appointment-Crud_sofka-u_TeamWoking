573e2cc37a4731db67b5d26ac67f452c12ea489f
Camilo U <cauribeva@unal.edu.co>
2021-09-14 20:43:54 -0500

    Agregados estilos a Form e index

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import CreateCardAppointment from './components/CreateCardAppointment'
+import GridAppointment from './components/GridAppointment'
 
 export const App = () => {
   return (
-    <div>
-      <h1>React TypeScript Webpack Starter Template</h1>
+    <div className="container-fluid my-3">
       <CreateCardAppointment />
+      <div className="container my-5  ">
+        <GridAppointment />
+      </div>
     </div>
   )
 }
--- src/components/CreateCardAppointment.tsx
+++ src/components/CreateCardAppointment.tsx
@@ -24,16 +24,36 @@ export default function CreateCardAppointment() {
   console.log(appointment)
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label>Doctor</label>
-      <input {...register('nameDoctor')} />
-      <label>Paciente</label>
-      <input {...register('namePatient')} />
-      <label>Motivo</label>
-      <input {...register('motiveConsulta')} />
-      <label>Lugar</label>
-      <input {...register('building')} />
-      <button type="submit">Enviar</button>
-    </form>
+    <div className="container">
+      <h1 className="h1 ">Reserva tu cita</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="row my-3">
+          <div className="col">
+            <div className="form-group">
+              <label>Doctor</label>
+              <input {...register('nameDoctor')} className="form-control" />
+            </div>
+            <div className="form-group">
+              <label className="form-check-label">Paciente</label>
+              <input {...register('namePatient')} className="form-control" />
+            </div>
+          </div>
+          <div className="col">
+            <div className="form-group">
+              <label>Motivo</label>
+              <input {...register('motiveConsulta')} className="form-control" />
+            </div>
+
+            <div className="form-group">
+              <label>Lugar</label>
+              <input {...register('building')} className="form-control" />
+            </div>
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Enviar
+        </button>
+      </form>
+    </div>
   )
 }
